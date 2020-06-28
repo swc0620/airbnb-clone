0466ca5c01e7c0832afc235cc742bd66ff990708
swc0620 <pinetreewave@naver.com>
2020-06-28 16:51:20 +0900

    15 Sign Up

--- users/models.py
+++ users/models.py
@@ -40,10 +40,18 @@ class User(AbstractUser):
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(null=True, blank=True)
     language = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+        choices=LANGUAGE_CHOICES, 
+        max_length=2, 
+        null=True, 
+        blank=True, 
+        default=LANGUAGE_KOREAN
     )
     currency = models.CharField(
-        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+        choices=CURRENCY_CHOICES, 
+        max_length=3, 
+        null=True, 
+        blank=True, 
+        default=CURRENCY_KRW
     )
     superhost = models.BooleanField(default=False)
 
