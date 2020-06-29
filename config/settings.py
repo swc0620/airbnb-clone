48ce6bc34580a001a513555f0ed5fc2cff838138
swc0620 <pinetreewave@naver.com>
2020-06-30 03:03:26 +0900

    19 Intro to TailwindCSS

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # We want to expand basic Django User Model with other properties
 AUTH_USER_MODEL = "users.User"
