f370cf492b7c1a7764396bdeaf3308fd21f63f7e
swc0620 <59383615+swc0620@users.noreply.github.com>
2020-07-13 11:30:10 +0900

    21 User Profile, Edit Profile, Change Password

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
+from django.shortcuts import reverse
 from django.template.loader import render_to_string
 
 # Create your models here.
@@ -92,3 +93,6 @@ class User(AbstractUser):
                 )
             self.save()
         return
+
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"pk": self.pk})
--- users/views.py
+++ users/views.py
@@ -1,13 +1,16 @@
 import os
 import requests
 from django.views import View
-from django.views.generic import FormView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.shortcuts import render, redirect, reverse
 from django.urls import reverse_lazy
-from django.contrib.auth import authenticate, login, logout
 from django.core.files.base import ContentFile
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.forms import UserCreationForm
-from . import forms, models
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from . import forms, models, mixins
 
 # Create your views here.
 
@@ -34,7 +37,7 @@ from . import forms, models
 #             "form": form,
 #         })
 
-class LoginView(FormView):
+class LoginView(mixins.LoggedOutOnlyView, FormView):
     template_name = "users/login.html"
     form_class = forms.LoginForm
 
@@ -43,7 +46,7 @@ class LoginView(FormView):
     # success_url = reverse("core:home")
 
     # reverse_lazay() is not immediately executed, but is executed only when the view is loaded
-    success_url = reverse_lazy("core:home")
+    # success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -53,7 +56,15 @@ class LoginView(FormView):
             login(self.request, user)
         return super().form_valid(form)
 
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
+
 def log_out(request):
+    messages.info(request, f"See you later {request.user.first_name}")
     logout(request)
     return redirect(reverse("core:home"))
 
@@ -117,7 +128,7 @@ def github_callback(request):
 
             error = token_json.get("error", None)
             if error is not None:
-                raise GithubException()
+                raise GithubException("Can't get access token")
             else:
                 # 3. You send the code to the github and again receive the access token to the user's github profiile
                 access_token = token_json.get("access_token")
@@ -141,7 +152,7 @@ def github_callback(request):
                     try:
                         user = models.User.objects.get(email=email)
                         if user.login_method != models.User.LOGIN_GITHUB:
-                            raise GithubException()
+                            raise GithubException(f"Please log in with: {user.login_method}")
 
                     # 5. If user with the github account does not exist, let him sign up
                     except models.User.DoesNotExist:
@@ -156,12 +167,14 @@ def github_callback(request):
                         user.set_unusable_password()
                         user.save()
                     login(request, user)
+                    messages.success(request, f"Welcome back {user.first_name}")
                     return redirect(reverse("core:home"))
                 else:
-                    raise GithubException()
+                    raise GithubException("Can't get your profile")
         else:
-            raise GithubException()
-    except GithubException:
+            raise GithubException("Can't get code")
+    except GithubException as e:
+        messages.error(request, e)
         # send error message
         return redirect(reverse("users:login"))
 
@@ -188,7 +201,7 @@ def kakao_callback(request):
 
         error = token_json.get("error", None)
         if error is not None:
-            raise KakaoException()
+            raise KakaoException("Can't get authorization code")
         else:
             access_token = token_json.get("access_token")
             profile_request = requests.get(
@@ -203,7 +216,7 @@ def kakao_callback(request):
             kakao_account = profile_json.get("kakao_account")
             email = kakao_account.get("email")
             if email is None:
-                raise KakaoException()
+                raise KakaoException("Please also give me your email")
             properties = profile_json.get("properties")
             nickname = properties.get("nickname")
             profile_image = properties.get("profile_image")
@@ -211,7 +224,7 @@ def kakao_callback(request):
             try:
                 user = models.User.objects.get(email=email)
                 if user.login_method != models.User.LOGIN_KAKAO:
-                    raise KakaoException()
+                    raise KakaoException(f"Please log in with: {user.login_method}")
             except models.User.DoesNotExist:
                 user = models.User.objects.create(
                     email=email,
@@ -231,7 +244,70 @@ def kakao_callback(request):
                     )
 
             login(request, user)
+            messages.success(request, f"Welcome back {user.first_name}")
             return redirect(reverse("core:home"))
-    except KakaoException:
+    except KakaoException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
-        pass
+
+class UserProfileView(DetailView):
+    
+    model = models.User
+    context_object_name = "user_obj"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["hello"] = "hello!"
+        return context
+
+class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
+    
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "email",
+        "first_name",
+        "last_name",
+        # "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+    success_message = "Profile Updated"
+
+    def get_object(self, queryset=None):
+        return self.request.user
+    
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        self.object.username = email
+        self.object.save()
+        return super().form_valid(form)
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields['email'].widget.attrs = {"placeholder": "Email"}
+        form.fields['first_name'].widget.attrs = {"placeholder": "First name"}
+        form.fields['last_name'].widget.attrs = {"placeholder": "Last name"}
+        form.fields['bio'].widget.attrs = {"placeholder": "Bio"}
+        form.fields['birthdate'].widget.attrs = {"placeholder": "Birthdate"}
+        form.fields['language'].widget.attrs = {"placeholder": "Language"}
+        form.fields['currency'].widget.attrs = {"placeholder": "Currency"}
+        return form
+
+class UpdatePassword(mixins.EmailLoginOnlyView, mixins.LoggedInOnlyView, SuccessMessageMixin, PasswordChangeView):
+
+    template_name = "users/update-password.html"
+    success_message = "Password Updated"
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields['old_password'].widget.attrs = {"placeholder": "Current password"}
+        form.fields['new_password1'].widget.attrs = {"placeholder": "New password"}
+        form.fields['new_password2'].widget.attrs = {"placeholder": "Confirm new password"}
+        return form
+    
+    def get_success_url(self):
+        return self.request.user.get_absolute_url()
